e5d9193790cfc0585266a380ced9387a31c1c7b2
Kevin Dao <kevin-dao@uiowa.edu>
2023-04-05 13:43:04 -0500

    Add firebase connection to server

--- server/src/index.js
+++ server/src/index.js
@@ -3,9 +3,29 @@ import { createServer } from 'http';
 import { Server } from 'socket.io';
 import express from 'express';
 import { Chess } from 'chess.js';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 import { initializeSocket } from './socket.js';
 import { startStockfish } from './stockfish.js';
+
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+export const database = getDatabase();
 
 export const game = new Chess();
 export let rooms = [];

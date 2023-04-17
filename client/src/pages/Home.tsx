d6dc3cc5de4c891a920a50058b4c8a7972f770f9
Kevin Dao <kevin-dao@uiowa.edu>
2023-04-17 14:19:58 -0500

    Display please login message when not logged in for home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,8 +6,13 @@ import OnlinePlayer from '../assets/images/online-player.png';
 import Button from '../components/Button';
 import PlayComputerDialog from '../components/Dialogs/PlayComputerDialog';
 import PlayOnlineDialog from '../components/Dialogs/PlayOnlineDialog';
+import { getAuth } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 export default function Home() {
+  const auth = getAuth();
+  const [user] = useAuthState(auth);
+
   const navigate = useNavigate();
 
   // dialog states
@@ -16,36 +21,39 @@ export default function Home() {
 
   return (
     <div className="container mx-auto">
-      <div className="flex items-center flex-col gap-2">
-        <div className="bg-dark-300 p-4 rounded-md w-[480px] space-y-4">
-          <div className="text-white text-2xl text-center font-bold">Play</div>
+      {user ? (
+        <div className="flex items-center flex-col gap-2">
+          <div className="bg-dark-300 p-4 rounded-md w-[480px] space-y-4">
+            <div className="text-white text-2xl text-center font-bold">
+              Play
+            </div>
 
-          <div className="flex gap-4">
-            {/* against online player */}
-            <button
-              onClick={() => setPlayOnlineOpen(true)}
-              className="text-white bg-dark-400 rounded-md p-2 flex flex-col items-center flex-1 hover:bg-dark-200"
-            >
-              <img
-                src={OnlinePlayer}
-                alt="online-player"
-                className="w-16 h-16"
-              />
-              <span>Play Online</span>
-            </button>
+            <div className="flex gap-4">
+              {/* against online player */}
+              <button
+                onClick={() => setPlayOnlineOpen(true)}
+                className="text-white bg-dark-400 rounded-md p-2 flex flex-col items-center flex-1 hover:bg-dark-200"
+              >
+                <img
+                  src={OnlinePlayer}
+                  alt="online-player"
+                  className="w-16 h-16"
+                />
+                <span>Play Online</span>
+              </button>
 
-            {/* against computer */}
-            <button
-              onClick={() => setPlayComputerOpen(true)}
-              className="text-white bg-dark-400 rounded-md p-2 flex flex-col items-center flex-1 hover:bg-dark-200"
-            >
-              <img src={Computer} alt="computer" className="w-16 h-16" />
-              <span>Play Computer</span>
-            </button>
+              {/* against computer */}
+              <button
+                onClick={() => setPlayComputerOpen(true)}
+                className="text-white bg-dark-400 rounded-md p-2 flex flex-col items-center flex-1 hover:bg-dark-200"
+              >
+                <img src={Computer} alt="computer" className="w-16 h-16" />
+                <span>Play Computer</span>
+              </button>
+            </div>
           </div>
-        </div>
 
-        {/* <div className="bg-dark-300 p-4 rounded-md w-[480px] space-y-4">
+          {/* <div className="bg-dark-300 p-4 rounded-md w-[480px] space-y-4">
           <div className="text-white text-2xl text-center font-bold">
             Join Room
           </div>
@@ -61,7 +69,14 @@ export default function Home() {
             </Button>
           </div>
         </div> */}
-      </div>
+        </div>
+      ) : (
+        <div>
+          <h1 className="text-white text-2xl text-center font-bold">
+            Welcome! Please login to continue
+          </h1>
+        </div>
+      )}
 
       {/* play online dialog */}
       <PlayOnlineDialog isOpen={playOnlineOpen} setIsOpen={setPlayOnlineOpen} />

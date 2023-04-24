21eb0e65527a5f51c5eb38bdc3e8519e19db1703
Kevin Dao <kevin-dao@uiowa.edu>
2023-04-23 19:45:06 -0500

    Add function to get pin mapping from shift register to led

--- pi/main.py
+++ pi/main.py
@@ -7,19 +7,19 @@ from firebase import db
 
 board = chess.Board()
 # board.set_fen("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq e6 0 1")
-picked_up = ['1']
+picked_up = []
 move_set = [['e2', 'e4']]
 
 
 LED_MAPPING = {
-    "a8": "56", "b8": "57", "c8": "58", "d8": "59", "e8": "60", "f8": "61", "g8": "62", "h8": "63",
-    "a7": "55", "b7": "54", "c7": "53", "d7": "52", "e7": "51", "f7": "50", "g7": "49", "h7": "48",
-    "a6": "40", "b6": "41", "c6": "42", "d6": "43", "e6": "44", "f6": "45", "g6": "46", "h6": "47",
-    "a5": "39", "b5": "38", "c5": "37", "d5": "36", "e5": "35", "f5": "34", "g5": "33", "h5": "32",
-    "a4": "24", "b4": "25", "c4": "26", "d4": "27", "e4": "28", "f4": "29", "g4": "30", "h4": "31",
-    "a3": "23", "b3": "22", "c3": "21", "d3": "20", "e3": "19", "f3": "18", "g3": "17", "h3": "16",
-    "a2":  "8", "b2":  "9", "c2": "10", "d2": "11", "e2": "12", "f2": "13", "g2": "14", "h2": "15",
-    "a1":  "7", "b1":  "6", "c1":  "5", "d1":  "4", "e1":  "3", "f1":  "2", "g1":  "1", "h1":  "0"
+    "a8": 56, "b8": 57, "c8": 58, "d8": 59, "e8": 60, "f8": 61, "g8": 62, "h8": 63,
+    "a7": 55, "b7": 54, "c7": 53, "d7": 52, "e7": 51, "f7": 50, "g7": 49, "h7": 48,
+    "a6": 40, "b6": 41, "c6": 42, "d6": 43, "e6": 44, "f6": 45, "g6": 46, "h6": 47,
+    "a5": 39, "b5": 38, "c5": 37, "d5": 36, "e5": 35, "f5": 34, "g5": 33, "h5": 32,
+    "a4": 24, "b4": 25, "c4": 26, "d4": 27, "e4": 28, "f4": 29, "g4": 30, "h4": 31,
+    "a3": 23, "b3": 22, "c3": 21, "d3": 20, "e3": 19, "f3": 18, "g3": 17, "h3": 16,
+    "a2":  8, "b2":  9, "c2": 10, "d2": 11, "e2": 12, "f2": 13, "g2": 14, "h2": 15,
+    "a1":  7, "b1":  6, "c1":  5, "d1":  4, "e1":  3, "f1":  2, "g1":  1, "h1":  0
 }
 
 
@@ -81,6 +81,16 @@ def get_all_legal_moves(board: chess.Board) -> dict:
   return moves
 
 
+def get_pin_mapping(input_pin: int) -> int:
+  row = input_pin // 8 + 1
+
+  # even row
+  if row % 2 == 0:
+    return input_pin
+  else:
+    return row * 8 - (input_pin % 8 + 1)
+
+
 if __name__ == '__main__':
   # initialize serial
   ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
@@ -104,12 +114,14 @@ if __name__ == '__main__':
     # time.sleep(10)
     # add_move('g1', 'f3')
     # time.sleep(10)
+    # add_move('d2', 'd4')
+    # time.sleep(10)
 
     if ser.in_waiting > 0:
       line = ser.readline().decode('utf-8').rstrip()
 
       if (line.startswith('PU')):
-        pin = line.split(' ')[1]
+        pin = get_pin_mapping(int(line.split(' ')[1]))
 
         # add pin to picked_up array if not in it
         if pin not in picked_up:
@@ -130,7 +142,8 @@ if __name__ == '__main__':
           square = [k for k, v in LED_MAPPING.items() if v == pin][0]
 
           # led to light up
-          led = " ".join([LED_MAPPING[move] for move in moves[square]]) + "\n"
+          led = " ".join([str(LED_MAPPING[move])
+                         for move in moves[square]]) + "\n"
 
           # send response to arduino
           res = led.encode('utf-8')
